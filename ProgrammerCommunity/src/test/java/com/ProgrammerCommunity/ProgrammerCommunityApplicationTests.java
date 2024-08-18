12328ea68683eb469060689e57237bab50c722fb
kimsungyun11 <163834021+kimsungyun11@users.noreply.github.com>
2024-08-18 21:46:02 +0900

    Add files via upload

---
+++ ProgrammerCommunity/src/test/java/com/ProgrammerCommunity/ProgrammerCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProgrammerCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
